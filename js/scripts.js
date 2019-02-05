15382b02e40f7dc91bbb010cf903b3de0631eefa
Guest User <Guest@epicodus-9.local>
2019-02-05 11:16:44 -0800

    l;l;l

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,41 @@
+function TriviaGame() {
+  this.triviaQuestions = [],
+  this.questionUsed = 0
+}
+
+function TriviaQuestions (imageURL, questionType, hint, answerOne, answerTwo, answerThree, answerFour, correctAnswer) {
+  this.imageURL = imageURL,
+  this.questionType = questionType,
+  this.hint = hint,
+  this.answerOne = answerOne,
+  this.answerTwo = answerTwo,
+  this.answerThree = answerThree,
+  this.answerFour = answerFour,
+  this.correctAnswer = correctAnswer
+}
+
 function processData(allText) {
     var allTextLines = allText.split(/\r\n|\n/);
     var headers = allTextLines[0].split(',');
     var lines = [];
 
 console.log(allTextLines);
-console.log(headers);
 
     for (var i=1; i<allTextLines.length; i++) {
         var data = allTextLines[i].split(',');
         if (data.length == headers.length) {
-            var tarr = [];
-            for (var j=0; j<headers.length; j++) {
-                tarr.push(headers[j]+":"+data[j]);
-            }
-            lines.push(tarr);
+            var triviaQuestions = new TriviaQuestions(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8]);
+
+//            for (var j=0; j<headers.length; j++) {
+//                tarr.push(data[j]);
+//            }
+//            lines.push(tarr);
         }
     }
-console.log(headers)
-console.log(tarr);
+console.log(triviaQuestions);
 }
+
+var triviaQuestions = new triviaQuestions;
 
 $(document).ready(function() {
 
